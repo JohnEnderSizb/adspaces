8f194e4e6dda49f50fa5b66884a5bd1c9b59f522
Johnson A.Siziba <siziba.uz@gmail.com>
2019-12-03 16:32:07 +0200

    working

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+
+def adspace_worpress_pugin(request):
+    template = loader.get_template('main/index.html')
+    return HttpResponse(template.render({}, request))
